16adec7b55021355fbb57e42fbbaf493b68afe60
wieczorekkr <wieczorekkr@gmail.com>
2013-01-24 00:08:39 +0100

    Implemented edit action for the tickets controller

--- features/step_definitions/creating_projects_steps.rb
+++ features/step_definitions/creating_projects_steps.rb
@@ -14,8 +14,14 @@ When /^I press "(.*?)"$/ do |arg1|
   click_button(arg1)
 end
 
-Then /^I should see "(.*?)"$/ do |arg1|
+Then /^I should see "([^"]*)"$/ do |arg1|
   assert(page.has_content?(arg1))
+end
+
+Then /^I should see "([^"]*)" within "(.*?)"$/ do |arg1, arg2|
+  within(arg2) do
+	assert(page.has_content?(arg1))
+  end
 end
 
 Then /^I should be on the project page for "(.*?)"$/ do |arg1|
--- features/step_definitions/viewing_projects_steps.rb
+++ features/step_definitions/viewing_projects_steps.rb
@@ -1,7 +1,13 @@
 Given /^there is a project called "(.*?)"$/ do |arg1|
-  Project.create(:name => arg1)
+  @project = Project.create(:name => arg1)
 end
 
 Then /^I should not see "(.*?)"$/ do |arg1|
   assert(page.has_no_content?(arg1))
+end
+
+Given /^that project has a ticket:$/ do |table|
+  	table.hashes.each do |attributes|
+		@project.tickets.create!(attributes)
+	end
 end
