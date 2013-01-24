a050bff520bd4c50511c052684c588b2f34170a7
wieczorekkr <wieczorekkr@gmail.com>
2013-01-24 23:03:37 +0100

    When creating tickets, attribute them to the creator.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124203020) do
+ActiveRecord::Schema.define(:version => 20130124205958) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20130124203020) do
     t.integer  "project_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   add_index "tickets", ["project_id"], :name => "index_tickets_on_project_id"
--- features/step_definitions/viewing_projects_steps.rb
+++ features/step_definitions/viewing_projects_steps.rb
@@ -5,9 +5,3 @@ end
 Then /^I should not see "(.*?)"$/ do |arg1|
   assert(page.has_no_content?(arg1))
 end
-
-Given /^that project has a ticket:$/ do |table|
-  	table.hashes.each do |attributes|
-		@project.tickets.create!(attributes)
-	end
-end
